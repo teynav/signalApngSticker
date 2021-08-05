c9b2d058826a5157f68fcf02a26128efb2077dcc
Navneet Vikram Tey <navneetvikramtey@gmail.com>
2021-08-06 01:48:15 +0530

    Added v3.0

---
+++ script_v3/download.py
@@ -0,0 +1,22 @@
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, CallbackContext
+f = open("token", "r")
+token=f.readline().split("\n")[0]
+bot= Bot(token)
+f = open("pack", "r")
+text = f.read()
+sticker_name = ""
+try:
+    sticker_name = text.split("/addstickers/")[1]
+except:
+    sticker_name = text.split("eu/stickers/")[1]
+sticker_set = bot.getStickerSet(sticker_name)
+ww=0
+ff = open("emoji", "w+")
+print("Downloading please wait!!")
+for i in sticker_set.stickers:
+   i.get_file().download(str(ww)+".tgs")
+   ww=ww+1
+   ff.write(i.emoji[:1]+"\n")
+    
+
