25296683faf90325e517c9d3b25b7a932c894003
Navneet Vikram Tey <navneetvikramtey@gmail.com>
2021-09-11 16:09:26 +0530

    Uploads large stickerpacks in chunks of 60

--- script_v3/bot.py
+++ script_v3/bot.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 import os
 import anyio
+import math
 from signalstickers_client import StickersClient
 from signalstickers_client.models import LocalStickerPack, Sticker
 from telegram import Update, Bot
@@ -15,49 +17,63 @@ async def main():
             stick.image_data = f_in.read()
 
         pack._addsticker(stick)
-
-    pack = LocalStickerPack()
+    def give_name(name, count, done):
+        if count == 1:
+            return name
+        return name+" Part "+str((done+1))
     f = open("token", "r")
     token=f.readline().split("\n")[0]
     author=f.readline().split("\n")[0]
     uuid= f.readline().split("\n")[0]
     password =f.readline().split("\n")[0]
-    pack.author = author
-    bot= Bot(token)
     f = open("pack", "r")
     text = f.read()
-    
     sticker_name = ""
+    emoji = open("emoji" , "r")
     try:
        sticker_name = text.split("/addstickers/")[1]
     except:
        sticker_name = text.split("eu/stickers/")[1]
+    bot= Bot(token)
     sticker_set = bot.getStickerSet(sticker_name)
-    pack.title = sticker_set.name
-    ww=0
-    emoji = open("emoji" , "r")
-    for i in sticker_set.stickers:
-       emoji_i = emoji.readline().split("\n")[0]
-       file = "result/"+str(ww)+".png"
-       add_sticker(file , emoji_i )
-#      print (file + " corresponds to " + emoji_i)
-       ww=ww+1
+    length=len(sticker_set.stickers)
+    chunk=60
+    totalloop=math.ceil(length/chunk)
+    doneloop=0
+    while totalloop-doneloop > 0 :
+        pack = LocalStickerPack()
+        pack.author = author
+        pack.title = give_name(sticker_set.name, totalloop , doneloop)
+        ww=chunk*doneloop
+        i=0
+        while i < 60:
+            totalfile=(chunk*doneloop)+i
+            if totalfile == length :
+                break
+            emoji_i = emoji.readline().split("\n")[0]
+            file = "result/"+str(ww)+".png"
+            add_sticker(file , emoji_i )
+            print (file + " corresponds to " + emoji_i)
+            ww=ww+1
+            i=i+1
     # Specifying a cover is optional
     # Instanciate the client with your Signal crendentials
-    print("-->")
-    print("Final stage Uploading is gonna start now!!!!!")
-    print ("token  ="+token )
-    print ("author ="+author )
-    print ("uuid (DO NOT SHARE)="+uuid)
-    print ("password (DO NOT SHARE)="+password)
-    print ("Pack name = "+ pack.title )
-    print ("-->")
-    ff=open("packs","a+")
-    async with StickersClient(uuid,password) as client:
-         pack_id, pack_key = await client.upload_pack(pack)
-    print("Pack uploaded!\n\nhttps://signal.art/addstickers/#pack_id={}&pack_key={}".format(pack_id, pack_key))
-    ff.write("### Pack = "+pack.title+"\n\nhttps://signal.art/addstickers/#pack_id={}&pack_key={}".format(pack_id, pack_key)+"\n---\n")
-    ff.close() 
+        print("-->")
+        print("Final stage Uploading is gonna start now!!!!!")
+        print ("token  ="+token )
+        print ("author ="+author )
+        print ("uuid (DO NOT SHARE)="+uuid)
+        print ("password (DO NOT SHARE)="+password)
+        print ("Pack name = "+ pack.title )
+        print ("-->")
+        ff=open("packs","a+")
+        async with StickersClient(uuid,password) as client:
+             pack_id, pack_key = await client.upload_pack(pack)
+        print("Pack uploaded!\n\nhttps://signal.art/addstickers/#pack_id={}&pack_key={}".format(pack_id, pack_key))
+        ff.write("### Pack = "+pack.title+"\n\nhttps://signal.art/addstickers/#pack_id={}&pack_key={}".format(pack_id, pack_key)+"\n---\n")
+        ff.close() 
+        doneloop=doneloop+1
+
 if __name__ == '__main__':
     anyio.run(main)
 
