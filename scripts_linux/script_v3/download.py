4b29ac1d357da0f17ccc56e7bbb16f4d4beda097
laggykiller <chaudominic2@gmail.com>
2022-12-01 18:34:28 +0800

    - Support for webm and webp stickers - Support for non-square stickers - Fix bug which cause pngquant to fail - Better handling of static stickers - Allow lower res and fps - Code identation cleanup

--- scripts_linux/script_v3/download.py
+++ scripts_linux/script_v3/download.py
@@ -1,10 +1,11 @@
 from telegram import Update, Bot
 from telegram.ext import Updater, CommandHandler, CallbackContext
-f = open("token", "r")
-token=f.readline().split("\n")[0]
+import os
+with open('token') as f:
+    token=f.readline().split("\n")[0]
 bot= Bot(token)
-f = open("pack", "r")
-text = f.read()
+with open('pack') as f:
+    text = f.read()
 sticker_name = ""
 try:
     sticker_name = text.split("/addstickers/")[1]
@@ -12,11 +13,10 @@ except:
     sticker_name = text.split("eu/stickers/")[1]
 sticker_set = bot.getStickerSet(sticker_name)
 ww=0
-ff = open("emoji", "w+")
 print("Downloading please wait!!")
-for i in sticker_set.stickers:
-   i.get_file().download(str(ww)+".tgs")
-   ww=ww+1
-   ff.write(i.emoji[:1]+"\n")
-    
-
+with open("emoji", "w+") as ff:
+    for i in sticker_set.stickers:
+        fname = i.get_file().download()
+        os.rename(fname, str(ww) + os.path.splitext(fname)[1])
+        ww=ww+1
+        ff.write(i.emoji[:1]+"\n")
